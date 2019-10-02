012b0acd9c0bbabe005ab6b0fa0776a5197ec22b
pc <pranjalcholadhara662@gmail.com>
2019-10-03 01:05:44 +0530

    Generic JSON Parser

---
+++ app/src/main/java/com/peasx/app/droidglobal/http/query/Column.java
@@ -0,0 +1,13 @@
+package com.peasx.app.droidglobal.http.query;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+    String name();
+}
